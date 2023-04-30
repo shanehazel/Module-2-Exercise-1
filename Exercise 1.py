1aa3cf68517b2e6615fe977af3e2bd5905e3769a
Shane <gupitshanehazel@gmail.com>
2023-05-01 04:35:41 +0800

    opening of file even and odd + checking all the even numbers

--- Exercise 1.py
+++ Exercise 1.py
@@ -6,8 +6,16 @@ def process ():
     with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
         # read numbers.txt line by line
         for line in input_file:
+            print(line.strip())
             input_num = int(line)
-        # if even,
-            # write to even.txt
-        # if odd,
-            # write to odd.txt
+            
+            # if even,
+            if input_num % 2 == 0:
+                # write to even.txt
+                output_even.write(str(input_num) + "\n")
+            
+            # if odd,
+                # write to odd.txt
+
+# end of method
+process()
