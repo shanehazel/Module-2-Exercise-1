ae34f1405d660ac64587c170f0da724ba1c903f8
Shane <gupitshanehazel@gmail.com>
2023-05-01 03:00:07 +0800

    pseudocode

---
+++ Exercise 1.py
@@ -0,0 +1,9 @@
+# pseudocode
+
+# make a method
+# open numbers.txt file (read), even.txt (append), odd.txt (append)
+# read numbers.txt line by line
+# if even,
+    # write to even.txt
+# if odd,
+    # write to odd.txt
