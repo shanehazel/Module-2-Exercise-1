66e19d922d71de9a3bbc6354c728c4e58f71975c
Shane <gupitshanehazel@gmail.com>
2023-05-01 04:42:03 +0800

    THE PROGRAM IS WORKINGgit add -A

--- Exercise 1.py
+++ Exercise 1.py
@@ -2,8 +2,10 @@
 
 # make a method
 def process ():
+
     # open numbers.txt file (read), even.txt (append), odd.txt (append)
     with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
+        
         # read numbers.txt line by line
         for line in input_file:
             print(line.strip())
