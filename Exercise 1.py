0dd0b00952c39d973eb8d1d060ac089ee2e8bf94
Shane <gupitshanehazel@gmail.com>
2023-05-01 03:17:49 +0800

    checking of every lines in the text file

--- Exercise 1.py
+++ Exercise 1.py
@@ -5,6 +5,8 @@ def process ():
     # open numbers.txt file (read), even.txt (append), odd.txt (append)
     with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
         # read numbers.txt line by line
+        for line in input_file:
+            input_num = int(line)
         # if even,
             # write to even.txt
         # if odd,
