50a88a49ac2f07c5831230e05defa56c0b701287
Shane <gupitshanehazel@gmail.com>
2023-05-01 03:14:36 +0800

    opening of file

--- Exercise 1.py
+++ Exercise 1.py
@@ -2,9 +2,10 @@
 
 # make a method
 def process ():
-# open numbers.txt file (read), even.txt (append), odd.txt (append)
-# read numbers.txt line by line
-# if even,
-    # write to even.txt
-# if odd,
-    # write to odd.txt
+    # open numbers.txt file (read), even.txt (append), odd.txt (append)
+    with open("numbers.txt") as input_file, open("even.txt", "a") as output_even, open("odd.txt", "a") as output_odd:
+        # read numbers.txt line by line
+        # if even,
+            # write to even.txt
+        # if odd,
+            # write to odd.txt
